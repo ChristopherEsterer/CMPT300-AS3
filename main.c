6bfd2b7e7f81f8ae27e38fa87f689622f8bb53c5
ChristopherEsterer <chris.esterer@gmail.com>
2020-07-12 16:46:10 -0700

    Added Main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+// Main .c file for CMPT 300 Assigment # 3.
+// Coded By Christopher Esterer (301116353)
+// and Gurjeet ** Fill in **
+// Program s-talk, talks to another instance of itself over UDP
+// sends data that is typed into one instance to the other instance and displays.
+// program call is in the form:
+// "s-talk [my port number] [remote machine name] [remote port number]"
+
+#include <pList.h> // .h file for the protected list ADT
+
+// global variable declaration
+
+protectedList inList;
+protectedList outList;
+
+void main(//arguments) // argument sintax from WS7
+{
+
+}
