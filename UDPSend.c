2c90094d2cfba50687331890cf2f7d85d5054e31
ChristopherEsterer <chris.esterer@gmail.com>
2020-07-12 16:51:17 -0700

    Added UPD Send.c and pasted sudocode from file

---
+++ UDPSend.c
@@ -0,0 +1,31 @@
+// UDP Send module .c file.
+// Coded by Chris Esterer and Gurjeet ***
+
+// **code Coppied from drivefile
+
+#include the UDP libraries and thread libraries
+#include the list.h library
+
+pthread thread; // declare a thread for this module
+List* outList; // pointer to the out list
+convar* sendConVar;
+
+UDPInit(PortInfo)
+{
+// some code to init the UDP Port for Sending
+// Then call the UDPSend Function?
+}
+UDPSend()
+{
+while(1)? // i think this needs to loop, maybe a 1 or maybe a run bool
+{
+sendConVar.wait();    // Wait until an signal from the keyboard Thread sendConVar
+// When there is something to send from the output list (there should be because we unblocked with the signal.
+msg = outList->GetMessageFromList() // ask the list for an item. The list will deal with mutexes.
+// Send the message over UDP to the outPort
+}
+}
+UDPThreadCreate() // passed in will be list, conVar, and port info
+{
+pthread_create(&thread, NULL, UDPInit, //Passed in stuff );
+}
