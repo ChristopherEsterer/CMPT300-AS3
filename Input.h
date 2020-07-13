4f41eee982d9ce80271b50bf544c1b035fffad16
ChristopherEsterer <chris.esterer@gmail.com>
2020-07-12 17:07:54 -0700

    Added Input .c/.h

---
+++ Input.h
@@ -0,0 +1,7 @@
+// .h file for input.c, the keyboard input module
+
+#include // pList?
+
+// function declaration
+void InputMain(void); // *** These all need work
+void InputThreadCreate(/* pList, thread handle, probs more */); 
