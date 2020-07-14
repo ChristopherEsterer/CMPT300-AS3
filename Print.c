51b97ccbf0dcfb137ec3350a797881693b5e3da4
ChristopherEsterer <chris.esterer@gmail.com>
2020-07-13 23:32:55 -0700

    Starting Print.c/.h

--- Print.c
+++ Print.c
@@ -2,9 +2,42 @@
 // and keep printing as long as something is in the list
 #include stdio // idk the call? ***
 #include <Print.h>
+#include <pthread.h>
+#include <signal.h>
 
-main(){
-receiveConVar_wait();
-messageToPrint = inList.GetMessage()// this is protected from the protectedList ADT
-printf(“%s /n”, messageToPrint);
+void* printerThread(void* unused){
+/* *** sudo
+    while(1) {
+        receiveConVar_wait();
+    }
+    receiveConVar_wait();
+    messageToPrint = inList.GetMessage()// this is protected from the protectedList ADT
+    printf(“%s /n”, messageToPrint);
+*/
+}
+
+void Printer_init(char* rxMessage)
+{
+    dynamicMessage = malloc(DYNAMIC_LEN);
+    
+    InitLists(); // Initalized the lists for memory allocation
+
+    s_rxMessage = rxMessage;
+    pthread_create(
+        &threadPID,         // PID (by pointer)
+        NULL,               // Attributes
+        receiveThread,      // Function
+        NULL);
+}
+
+void Printer_shutdown(void)
+{
+    // Cancel thread
+    pthread_cancel(threadPID);
+
+    // Waits for thread to finish
+    pthread_join(threadPID, NULL);
+
+    // Cleanup memory
+  
 }
--- Print.h
+++ Print.h
@@ -1,6 +1,11 @@
 // .h File for Print.c
 // create as thread for the printing module
+#ifndef _PRINTER_H_
+#define _PRINTER_H_
 
-#include //plist, thread,
 
 void PrintThreadCreate(void); // *** to do
+void Printer_init(void); // Initialize thread call
+void Printer_shutdown(void); // Printer Shutdown
+
+#endif
