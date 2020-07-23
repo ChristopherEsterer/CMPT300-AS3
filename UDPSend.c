31b5b93c7718643bd16e9c08f6c22ce0652d96d6
gurjeetsingh <gurjeetsg1@gmail.com>
2020-07-22 22:52:54 -0700

    overhaul of code, similar but copy paste in showing big change in lines

--- UDPSend.c
+++ UDPSend.c
@@ -52,52 +52,49 @@ void* SendThread(void* unused)
     //ie:get adddrinfo setup
     struct addrinfo hints;
            struct addrinfo *result, *rp;
-           int sfd, s;
-           struct sockaddr_storage peer_addr;
-           socklen_t peer_addr_len;
+           int sfd, s, j;
+           size_t len;
            ssize_t nread;
            char buf[BUF_SIZE];
 
-           // ** error checking - cant seam to get it to work
-           /* if (argc != 2) {
-               fprintf(stderr, "Usage: %s port\n", argv[0]);
+           if (argc < 3) {
+               fprintf(stderr, "Usage: %s host port msg...\n", argv[0]);
                exit(EXIT_FAILURE);
-           } */
+           }
+
+           /* Obtain address(es) matching host/port */
 
            memset(&hints, 0, sizeof(struct addrinfo));
-           hints.ai_family = AF_INET;    /* Allow IPv4*/
+           hints.ai_family = AF_INET;    /* Allow IPv4 only */
            hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
-           hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+           hints.ai_flags = 0;
            hints.ai_protocol = 0;          /* Any protocol */
-           hints.ai_canonname = NULL;
-           hints.ai_addr = NULL;
-           hints.ai_next = NULL;
 
-           s = getaddrinfo(NULL, argv[1], &hints, &result);
+           s = getaddrinfo(argv[1], argv[2], &hints, &result);
            if (s != 0) {
                fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
                exit(EXIT_FAILURE);
            }
 
            /* getaddrinfo() returns a list of address structures.
-              Try each address until we successfully bind(2).
-              If socket(2) (or bind(2)) fails, we (close the socket
+              Try each address until we successfully connect(2).
+              If socket(2) (or connect(2)) fails, we (close the socket
               and) try the next address. */
 
            for (rp = result; rp != NULL; rp = rp->ai_next) {
                sfd = socket(rp->ai_family, rp->ai_socktype,
-                       rp->ai_protocol);
+                            rp->ai_protocol);
                if (sfd == -1)
                    continue;
 
-               if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0)
+               if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
                    break;                  /* Success */
 
                close(sfd);
            }
 
            if (rp == NULL) {               /* No address succeeded */
-               fprintf(stderr, "Could not bind\n");
+               fprintf(stderr, "Could not connect\n");
                exit(EXIT_FAILURE);
            }
 
