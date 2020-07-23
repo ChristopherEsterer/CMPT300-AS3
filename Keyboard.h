d1042c4180816f0d05d9b585e0fff8546a71a95c
gurjeetsingh <gurjeetsg1@gmail.com>
2020-07-23 15:11:58 -0700

    clean up comments

--- Keyboard.h
+++ Keyboard.h
@@ -4,10 +4,9 @@
 #define _KEYBOARD_H_
 
 
-void* KeyboardThread(void* unused); // *** to do
+void* KeyboardThread(void* unused); 
 void KeyboardInit(void); // Initialize thread call
 void KeyboardShutdown(void); // Keyboard Shutdown
-//void KeyboardSignalMessage(void); // Mutex lock for the condition variable. This is the external signal call
 
 #endif
  
