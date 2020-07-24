dacfaf9983d5d4fd8eeb215fc2d6b2fa24afedb0
gurjeetsingh <gurjeetsg1@gmail.com>
2020-07-24 15:31:01 -0700

    comments formatting

--- ProtectedList.h
+++ ProtectedList.h
@@ -4,14 +4,14 @@
 // static pthread_mutex_t listLockMutex
 
 // Function declaration
-char* GetMessageFromInputList(void); // get a Message from the list, wraps the list read in mutexes
+char* GetMessageFromInputList(void); //get a Message from the list, wraps the list read in mutexes
 void GetMessageFromInputList2(char*);
 char* GetMessageFromOutputList(void);
-void SetMessageToInputList(char* msg); // adds a message to the list, protects the list push call.
+void SetMessageToInputList(char* msg); //adds a message to the list, protects the list push call.
 void SetMessageToOutputList(char* msg);
 
 int GetListSize(void);
 
-void SignalPrintMsg(void); //
-void ListInit(void); // allocate list memory
+void SignalPrintMsg(void); //Signals printer
+void ListInit(void); //allocate list memory
 void ListShutdown(void);
