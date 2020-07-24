81e653eaae91ced4f3204b2b855555c3b9222d5a
gurjeetsingh <gurjeetsg1@gmail.com>
2020-07-24 15:37:49 -0700

    comment formatting

--- UDPSend.h
+++ UDPSend.h
@@ -1,23 +1,14 @@
-//pseudocode from drive
-/* 
-// .h file for UDPSend Module
-
-// Function Declarations
-void UDPSend(void); // UDPSend function ***
-void UDPThreadCreate( pList, conVar, portinfo); // Creates thread for UDP send ***
- */
-
-// .h file for UDPSend module
-// some code from Workshops
+//.h file for UDPSend module
+//some code from Workshops
 
 
-// Start background send thread
-// void Receiver_init(char* rxMessage, pthread_mutex_t sharedMutexWithOtherThread);
+//Start background send thread
+//void Receiver_init(char* rxMessage, pthread_mutex_t sharedMutexWithOtherThread);
 void SenderInit(char* addr , char* port);
 void* SendThread(void* unused);
-// Set the dynamic message -- Threadsafe
+//Set the dynamic message -- Threadsafe
 void SenderChangeDynamicMessage(char* txMessage);
 void SenderSignalMessage(void);
-// Stop background receive thread and cleanup
+//Stop background receive thread and cleanup
 void SenderShutdown(void);
 
